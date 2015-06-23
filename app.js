cbc623dac6286e38e0cf3f74ccdbc1c28ada0066
Dan Proudfoot <dan.proudfoot.iw@gmail.com>
2015-06-23 01:20:38 +0100

    Readying for push

---
+++ app.js
@@ -0,0 +1,75 @@
+var json = require('jsonfile');
+var T = require('twit');
+var express = require('express');
+var BodyParser = require('body-parser');
+var cors = require('cors');
+
+var file = './list.json';
+
+var app = express();
+var router = express.Router();
+
+app.use(BodyParser.urlencoded({extended: false}));
+app.use(cors());
+
+var messages = [];
+var defaultMessages = [
+	"Here's a test one",
+	"Here's another test",
+	"Here's the final one"
+];
+
+Array.prototype.move = function(from, to) {
+    this.splice(to, 0, this.splice(from, 1)[0]);
+};
+
+function pusher(message, keep){
+	messages.push({
+		message:message,
+		keep: keep
+	});
+	// json.writeFile(file,messages,function(err){
+	// 	if (err){ 
+	// 		console.log(err)
+	// 	} else {
+	// 		console.log("Message Saved")
+
+	// 	};
+	// })
+}
+
+app.get('/', function(req, res){
+	
+	if (!messages.length == 0) {
+		res.send(messages[0].message);
+		console.log(messages);
+		
+		if (messages[0].keep == 1){
+			console.log(messages[0]);
+			messages[0].keep = 0;
+			messages.move(0,messages.length -1 );	
+		} else if (messages[0].keep == 0) {
+			messages.splice(0,1);
+		} else if (messages[0].keep == -1){
+
+		}
+
+	} else {
+		var rand = Math.floor(Math.random() * defaultMessages.length);
+		res.send(defaultMessages[rand]);
+	}
+});
+
+app.post('/', function(req, res){
+	var message = req.body.message;
+	var keep = req.body.keep;
+	console.log(message + " --Keep? " + keep);
+	pusher(message, keep);
+	res.send("Message Sent");
+})
+
+app.use('/', router);
+
+
+app.listen(3120);
+console.log("Running on port 3120");
---
+++ client.js
@@ -0,0 +1,25 @@
+$(window).load(function() {
+
+	iteration = 0;
+	function repeat(){
+		$('.marquee_text')
+
+			.bind('finished', function (){
+				var self = $(this);
+				$.get('http://localhost:3120/', function(data, status){
+					self.marquee('destroy');
+					self.html(data)
+					console.log("Iteration: " + iteration + " -- " + data);
+					iteration++;
+					repeat();
+				});
+			})
+
+			.marquee({
+				duration: 7000
+			});	
+	}
+
+	repeat();
+
+});
